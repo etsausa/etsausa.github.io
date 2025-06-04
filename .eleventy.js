0de168c0a3efbdc6a05e8063d8aab14afb662af3
Ethan <etsausa@gmail.com>
2025-06-04 17:43:25 -0400

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+module.exports = function(eleventyConfig){
+    eleventyConfig.addPassthroughCopy("src/assets/");
+    eleventyConfig.addWatchTarget("src/assets/");
+    eleventyConfig.addPassthroughCopy("src/css/");
+    eleventyConfig.addWatchTarget("src/css/");
+
+
+    return {
+        dir: {
+            input: 'src',
+            includes: '_includes',
+            output: '_site',
+        },
+        templateFormats: [ 'md', 'njk', 'html'],
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk'
+    }
+}
